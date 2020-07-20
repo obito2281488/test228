c3f5dad8f7ed7dfc7d5a0af24a3c4ed7b6146445
denis <depotch2@yandex.ru>
2020-07-20 15:43:38 +0300

    migrations

---
+++ base/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='media/category/', null=True, blank=True)
+    is_visible = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    is_visible = models.BooleanField(default=True)
+    price = models.FloatField()
+    weight = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    main_image = models.ImageField(upload_to='media/product/')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class OrderProducts(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.DO_NOTHING)
+    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
+    product_amount = models.IntegerField(default=1)
--- vkus/settings.py
+++ vkus/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'admin_site'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'vkus.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'vkus.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'vkus_menu',
+        'USER': 'denis',
+        'PASSWORD': 'denis1998',
+        'HOST': '127.0.0.1',
+        'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
     }
 }
 
